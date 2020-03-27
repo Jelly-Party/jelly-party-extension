c427a0b79bdaff126a884da73a4d5b23912a6881
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-03-27 10:31:11 +0100

    added options page

--- options.js
+++ options.js
@@ -7,9 +7,10 @@ $(function () {
     var s1 = $("#customSwitch1")[0];
     var s2 = $("#customSwitch2")[0];
     var name = $("#usernameTextfield")[0];
+    var url = $("#configuredURL")[0]
     // Handle button-save-options click
     $("#button-save-options").click(function (e) {
-        var options = { name: (name.value) ? name.value : "guest", s1: s1.checked, s2: s2.checked };
+        var options = { name: (name.value) ? name.value : "guest", url: url.value, s1: s1.checked, s2: s2.checked };
         chrome.storage.sync.set({ options: options }, function () {
             console.log('Options have been set:');
             console.log(options);
@@ -23,7 +24,8 @@ $(function () {
         chrome.storage.sync.get(["options"], function (res) {
             s1.checked = res.options.s1;
             s2.checked = res.options.s2;
-            name.value = (res.options.name) ? res.options.name : "guest";
+            url.value = res.options.url ? res.options.url : "";
+            name.value = res.options.name ? res.options.name : "guest";
 
         })
     }
