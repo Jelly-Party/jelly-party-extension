3cafbd088734dc10d30de6f45172d7f39c87ea29
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-03-27 10:31:51 +0100

    updated background.js

--- background.js
+++ background.js
@@ -1,41 +1,57 @@
 chrome.runtime.onInstalled.addListener(function () {
-  var options = { name: "guest", s1: true, s2: true };
+  var options = { name: "guest", url: "www.example.com", s1: true, s2: true };
+  // For debuggin only!!
+  ///////////////////////////////////////////////////////////////////////////////
+  var options = { name: "sean", url: "media.eulenberg.de", s1: true, s2: true };
+  ///////////////////////////////////////////////////////////////////////////////
   chrome.storage.sync.set({ options: options }, function () {
     console.log('Options have been initialized.');
   });
-
-});
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  // TODO: here we must update the pageUrl
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'media.eulenberg.de' },
-      })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
 });
 
-chrome.runtime.onMessage.addListener(function (message, callback) {
-  if (message == 'hello') {
-    console.log("hello");
-  } else if (message == 'goodbye') {
-    chrome.runtime.Port.disconnect();
-  }
-});
 
-// Partys are managed in the background. Therefore party state must be synced between background.js and popup.html/js
 chrome.webNavigation.onCompleted.addListener(function () {
-  console.log("This is my favorite website!");
-  var port = chrome.runtime.connect({ name: "backgroundAndContent" });
-  port.postMessage({ joke: "Knock knock" });
-  port.onMessage.addListener(function (msg) {
-    if (msg.question == "Who's there?")
-      port.postMessage({ answer: "Madame" });
-    else if (msg.question == "Madame who?")
-      port.postMessage({ answer: "Madame... Bovary" });
-  });
+  chrome.storage.sync.get(["options"], function (result) {
+    var targetUrl = result.options.url;
+    chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
+      var currentUrl = tabs[0].url;
+      if (currentUrl) {
+        if (currentUrl.includes(targetUrl)) {
+          console.log(`We're on fire! Injecting script for ${currentUrl}`);
+          chrome.tabs.executeScript({
+            file: 'js-libs/peerjs.min.js'
+          });
+          chrome.tabs.executeScript({
+            file: 'js-libs/uuidv4.min.js'
+          });
+          chrome.tabs.executeScript({
+            file: 'contentScript.js'
+          });
+        } else {
+          console.log(`Not injecting script for ${currentUrl}`)
+        }
+      }
+    });
+  })
 
-}, { url: [{ hostEquals: 'media.eulenberg.de' }] });
+}, { url: [{ urlPrefix: "http" }] });
+
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  chrome.storage.sync.get(["options"], function (result) {
+    var url = result.options.url;
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            hostEquals: url
+          },
+        })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+
+  })
+});
+
