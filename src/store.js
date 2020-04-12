e1da9cc516756c47e01adfaf03454d22922d381b
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-04-12 15:13:10 +0200

    Fixed store update

--- src/store.js
+++ src/store.js
@@ -11,6 +11,13 @@ export default {
     magicLink: ""
   },
   updateState: function (newState) {
-    this.state = { ...this.state, ...newState }
+    this.state.isActive = newState.isActive
+    this.state.partyId = newState.partyId
+    this.state.peers = newState.peers
+    this.state.websocketConnection = newState.websocketConnection
+    this.state.lastPartyId = newState.lastPartyId
+    this.state.websiteIsTested = newState.websiteIsTested
+    this.state.name = newState.name
+    this.state.magicLink = newState.magicLink
   }
 }
