82257871c6beefd620b2c1880e4132d8fab6d279
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-04-13 15:53:49 +0200

    added files to public folder

---
+++ public/background.js
@@ -0,0 +1,19 @@
+chrome.runtime.onInstalled.addListener(function() {
+  var options = { name: "guest" };
+  chrome.storage.sync.set({ options: options }, function() {
+    console.log("Options have been initialized.");
+  });
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([
+    {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          css: ["video"]
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }
+  ]);
+});
