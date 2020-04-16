b0cbef8953f85517674cc919c721ac5936b3d0e2
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-04-16 15:45:06 +0200

    added staging/dev/prod flows

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <h3 class="text-white mt-0 p-4 text-center font-weight-bold brand-header">
-    Jelly Party
+    {{ title }}
   </h3>
 </template>
 
@@ -9,3 +9,10 @@
   background: linear-gradient(to bottom right, #ff9494 0%, #ee64f6 100%);
 }
 </style>
+<script>
+export default {
+  data: function() {
+    return { title: process.env.VUE_APP_TITLE }
+  }
+}
+</script>
