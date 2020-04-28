0bb25b0167ce55d99e05d30368e02a11ddc7c6e9
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-04-28 11:40:09 +0200

    add close-button for popup to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,38 @@
 <template>
-  <h3 class="text-white mt-0 p-4 text-center font-weight-bold brand-header">
-    {{ title }}
-  </h3>
+  <div class="brand-header">
+    <span
+      id="close-button"
+      class="font-weight-bold btn text-white p-0"
+      style="font-size:1.3em; position: absolute; top: 6px; right: 6px"
+      v-on:click="close"
+      ><b-icon-x></b-icon-x
+    ></span>
+    <h3 class="text-white mt-0 p-4 text-center font-weight-bold">
+      {{ title }}
+    </h3>
+  </div>
 </template>
 
 <style scoped>
 .brand-header {
   background: linear-gradient(to bottom right, #ff9494 0%, #ee64f6 100%);
 }
+#close-button {
+  transition: transform 0.2s ease-in-out;
+}
+#close-button:hover {
+  transform: rotate(360deg);
+}
 </style>
 <script>
 export default {
   data: function() {
     return { title: process.env.VUE_APP_TITLE };
-  }
+  },
+  methods: {
+    close: function() {
+      window.close();
+    },
+  },
 };
 </script>
