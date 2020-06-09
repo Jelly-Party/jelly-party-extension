08d2075d7e9259937019b13dcb1fe52a2eef8e31
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-06-09 16:37:51 +0200

    update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,18 @@ module.exports = {
   env: {
     node: true,
     webextensions: true,
-    browser: true
+    browser: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-unused-vars": [
       "error",
-      { vars: "all", args: "none", ignoreRestSiblings: false }
-    ]
-  }
+      { vars: "all", args: "none", ignoreRestSiblings: false },
+    ],
+  },
 };
