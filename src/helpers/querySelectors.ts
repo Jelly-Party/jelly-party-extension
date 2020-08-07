0a99c66d0485b856129c928bdea8224beec77c52
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-08-07 19:00:36 +0200

    add smarter query selectors

--- src/helpers/querySelectors.ts
+++ src/helpers/querySelectors.ts
@@ -4,6 +4,15 @@ export function deepQuerySelectorAll(selector: string) {
     .querySelectorAll(selector)
     .forEach((elem: Element) => elements.push(elem));
   document.querySelectorAll("iframe").forEach(iframe => {
+    // Let's check that the iframe is same origin. Otherwise, this is a lost cause anyways.
+    try {
+      if (!(new URL(iframe.src).host === window.location.host)) {
+        console.log("Jelly-Party: Not scanning cross-origin iFrame.");
+        return;
+      }
+    } catch {
+      return;
+    }
     const iframeElements = (iframe as HTMLIFrameElement).contentWindow?.document.querySelectorAll(
       selector,
     );
@@ -17,5 +26,19 @@ export function deepQuerySelectorAll(selector: string) {
 }
 
 export function querySelector(selector: string) {
-  return document.querySelector(selector) as HTMLElement;
+  return (
+    (document.querySelector(selector) as HTMLElement) ??
+    (document.createElement("div") as HTMLElement)
+  );
+}
+
+export function getReferenceToLargestVideo() {
+  const videos = (deepQuerySelectorAll("video") as unknown) as Array<
+    HTMLVideoElement
+  >;
+  const videoSizes = Array.from(videos).map(
+    v => v.offsetHeight * v.offsetWidth,
+  );
+  const maxIndex = videoSizes.indexOf(Math.max(...videoSizes));
+  return videos[maxIndex];
 }
